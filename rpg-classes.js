c275fdb4ae88a38eab92cb93a1792f249e0bcedf
Garrett Cox <gacx89@gmail.com>
2020-12-10 09:35:05 -0600

    first commit

---
+++ rpg-classes.js
@@ -0,0 +1,7 @@
+// Create a class named Character, with name, strength and hitpoints properties and attack(), printStats() and isAlive() methods 
+
+// =============================================================
+
+// Create two new instances of a `Character`, giving them different names, strength, and hitPoints.
+
+// Call a combination of `printStats()`, `attack()`, and `isAlive()` methods to have the two characters "fight" in your console.
